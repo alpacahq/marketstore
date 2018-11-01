84443198cf705d8b5ac30055ab23bb0908a4d126
Luke Lonergan <spammy2013-100001@yahoo.com>
2018-11-01 10:27:49 -0700

    Add test for loader time parsing

---
+++ cmd/connect/loader/all_test.go
@@ -0,0 +1,28 @@
+package loader
+
+import (
+	. "gopkg.in/check.v1"
+	"testing"
+	"time"
+)
+
+// Hook up gocheck into the "go test" runner.
+func Test(t *testing.T) { TestingT(t) }
+
+type LoaderTests struct{}
+var _ = Suite(&LoaderTests{})
+func (s *LoaderTests) SetUpSuite(c *C) {}
+func (s *LoaderTests) TearDownSuite(c *C) {}
+
+func (s *LoaderTests) TestParseTime(c *C) {
+	tt := time.Date(2016, 12, 30, 21, 59, 20, 383000000,time.UTC)
+	var fAdj int
+	timeFormat := "20060102 15:04:05"
+	dateTime := "20161230 21:59:20 383000"
+	tzLoc := time.UTC
+	tTest, err := parseTime(timeFormat, dateTime, tzLoc, fAdj)
+	c.Assert(err != nil, Equals, true)
+	formatAdj := len(dateTime) - len(timeFormat)
+	tTest, err = parseTime(timeFormat, dateTime, tzLoc, formatAdj)
+	c.Assert(tt == tTest, Equals, true)
+}
