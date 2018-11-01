95ee8db538be3dc65446044949cbfddc80c63a7c
Luke Lonergan <spammy2013-100001@yahoo.com>
2018-11-01 16:10:48 -0700

    Fix tests

--- tests/integ/bin/runtests.sh
+++ tests/integ/bin/runtests.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 rm -rf testdata test_ticks.csv && mkdir -p testdata/mktsdb
-
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 marketstore connect -d `pwd`/testdata/mktsdb <<- EOF
 \create TEST/1Min/TICK:Symbol/Timeframe/AttributeGroup Bid,Ask/float32 variable
@@ -13,39 +10,40 @@ marketstore connect -d `pwd`/testdata/mktsdb <<- EOF
 \o test_ticks.csv
 \show TEST/1Min/TICK 1970-01-01
 EOF
-
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 diff -q bin/ticks-example-1-output.csv test_ticks.csv && echo "Passed"
-
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 rm -f test_ticks.csv
-
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 marketstore connect -d `pwd`/testdata/mktsdb <<- EOF
-\getinfo TEST/1Min/TICK
-\load TEST/1Min/TICK bin/ticks-example-2.csv bin/ticks-example-2.yaml
+\getinfo TEST2/1Min/TICK
+\create TEST2/1Min/TICK:Symbol/Timeframe/AttributeGroup Bid,Ask/float32 variable
+\load TEST2/1Min/TICK bin/ticks-example-2.csv bin/ticks-example-2.yaml
 \o test_ticks.csv
-\show TEST/1Min/TICK 1970-01-01
+\show TEST2/1Min/TICK 1970-01-01
 EOF
-
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+if [ $? -ne 0 ]; then exit 1; fi
 
 diff -q bin/ticks-example-2-output.csv test_ticks.csv && echo "Passed"
+if [ $? -ne 0 ]; then exit 1; fi
 
-if [ $? -ne 0 ]; then
-  exit 1
-fi
+rm -f test_ticks.csv
+if [ $? -ne 0 ]; then exit 1; fi
 
-rm -rf testdata test_ticks.csv
+marketstore connect -d `pwd`/testdata/mktsdb <<- EOF
+\getinfo TEST2/1Min/TICK
+\create TEST2/1Min/TICK:Symbol/Timeframe/AttributeGroup Bid,Ask/float32 variable
+\load TEST2/1Min/TICK bin/ticks-example-1.csv bin/ticks-example-1.yaml
+\o test_ticks.csv
+\show TEST2/1Min/TICK 1970-01-01
+EOF
+if [ $? -ne 0 ]; then exit 1; fi
 
+cat bin/ticks-example-1-output.csv bin/ticks-example-2-output.csv > tmp.csv
+diff -q tmp.csv test_ticks.csv && echo "Passed"
+if [ $? -ne 0 ]; then exit 1; fi
+
+rm -rf testdata test_ticks.csv tmp.csv
