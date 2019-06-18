5a52b562128039a5648eed0f188880d3cc01b463
Luke Lonergan <luke@alpaca.markets>
2019-06-18 14:31:28 -0700

    Go mod tidy and go fmt

--- cmd/estimate/main.go
+++ cmd/estimate/main.go
@@ -7,13 +7,14 @@ import (
 )
 
 const (
-	usage                 = "estimate"
-	short                 = "Estimate required storage space"
-	long                  = short
-	example               = "marketstore estimate --symbols 5000 --timeframe 1Sec --years 5"
+	usage   = "estimate"
+	short   = "Estimate required storage space"
+	long    = short
+	example = "marketstore estimate --symbols 5000 --timeframe 1Sec --years 5"
 
-	headerBytes           = 37024
+	headerBytes = 37024
 )
+
 var intervalsPerDay = map[string]int64{
 	"1D":   1,
 	"1Min": 24 * 60,
@@ -24,19 +25,19 @@ var intervalsPerDay = map[string]int64{
 
 var (
 	Cmd = &cobra.Command{
-		Use:        usage,
-		Short:      short,
-		Long:       long,
-		Example:    example,
-		RunE:       executeStart,
+		Use:     usage,
+		Short:   short,
+		Long:    long,
+		Example: example,
+		RunE:    executeStart,
 	}
-	Num4ByteCols    int64
-	Num8ByteCols    int64
-	Timeframe       string
-	NumSymbols      int64
-	NumYears        int64
-	HoursPerDay     float64
-	DaysPerYear     int64
+	Num4ByteCols int64
+	Num8ByteCols int64
+	Timeframe    string
+	NumSymbols   int64
+	NumYears     int64
+	HoursPerDay  float64
+	DaysPerYear  int64
 )
 
 func init() {
@@ -50,7 +51,7 @@ func init() {
 		"Number of symbols stored")
 	Cmd.Flags().Int64VarP(&NumYears, "years", "y", 10,
 		"Number of years worth of data to store")
-	Cmd.Flags().Int64VarP(&DaysPerYear, "days","d", 261,
+	Cmd.Flags().Int64VarP(&DaysPerYear, "days", "d", 261,
 		"Number of trading days in a year")
 	Cmd.Flags().Float64VarP(&HoursPerDay, "hours", "", 6.5,
 		"Number of hours per day the market is open")
@@ -58,26 +59,25 @@ func init() {
 
 func executeStart(cmd *cobra.Command, args []string) error {
 	var (
-		recordBytes    int64
-		padding        int64
-		yearFraction   float64
-		fileBytes      float64
-		totalBytes     float64
+		recordBytes  int64
+		padding      int64
+		yearFraction float64
+		fileBytes    float64
+		totalBytes   float64
 	)
 
-	recordBytes = 8 + (Num4ByteCols * 4) + (Num8ByteCols * 8)  // +8 for the index
+	recordBytes = 8 + (Num4ByteCols * 4) + (Num8ByteCols * 8) // +8 for the index
 	padding = int64(math.Mod(float64(recordBytes), 8))
 	recordBytes = recordBytes + padding
 
 	yearFraction = float64(DaysPerYear) * (HoursPerDay / 24.0)
-	fileBytes = float64(headerBytes) + (
-		float64(recordBytes) * float64(intervalsPerDay[Timeframe]) * yearFraction)
+	fileBytes = float64(headerBytes) + (float64(recordBytes) * float64(intervalsPerDay[Timeframe]) * yearFraction)
 
 	totalBytes = fileBytes * float64(NumSymbols) * float64(NumYears)
 
 	/*
 		Print the results and exit
-	 */
+	*/
 	symbolStr := "symbols"
 	if NumSymbols == 1 {
 		symbolStr = "symbol"
@@ -88,12 +88,12 @@ func executeStart(cmd *cobra.Command, args []string) error {
 	}
 	var sizes = []string{"KB", "MB", "GB", "TB", "PB", "EB"}
 	for i := range sizes {
-		sizeBytes := math.Pow(10, float64((i + 1) * 3))
+		sizeBytes := math.Pow(10, float64((i+1)*3))
 
 		if totalBytes < (sizeBytes * 10000) {
 			fmt.Printf(
 				"Estimated space required for %d %s with %d %s of %s data: %.0f%s\n",
-				NumSymbols, symbolStr, NumYears, yearStr, Timeframe, totalBytes / sizeBytes, sizes[i],
+				NumSymbols, symbolStr, NumYears, yearStr, Timeframe, totalBytes/sizeBytes, sizes[i],
 			)
 			return nil
 		}
