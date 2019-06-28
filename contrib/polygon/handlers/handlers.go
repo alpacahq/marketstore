6b9ff36e20a5b9b16051983a4ca0dc5d926621cf
Luke Lonergan <luke@alpaca.markets>
2019-06-28 10:58:26 -0700

    WIP: refactor

--- contrib/polygon/handlers/handlers.go
+++ contrib/polygon/handlers/handlers.go
@@ -3,16 +3,15 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/alpacahq/marketstore/contrib/polygon/api"
+	"github.com/alpacahq/marketstore/executor"
+	"github.com/alpacahq/marketstore/utils/io"
+	"github.com/alpacahq/marketstore/utils/log"
+	"github.com/buger/jsonparser"
+	"github.com/nats-io/go-nats"
 	"strings"
+	"sync"
 	"time"
-
-	"github.com/alpacahq/gopaca/log"
-	"github.com/alpacahq/polycache/enum"
-
-	"github.com/alpacahq/polycache/utils"
-
-	"github.com/alpacahq/polycache/raft"
-	"github.com/alpacahq/polycache/structures"
 )
 
 const (
@@ -24,13 +23,24 @@ const (
 	ConditionOpening         = 19
 )
 
+func conditionsPresent(conditions []int) (skip bool) {
+		for _, c := range conditions {
+			switch c {
+			case ConditionExchangeSummary, ConditionReOpening, ConditionOpening, ConditionClosing,
+			OfficialConditionOpening, OfficialConditionClosing:
+				return true
+			}
+		}
+		return
+}
+
 // TradeHandler handles a Polygon WS trade
 // message and stores it to the cache
 func TradeHandler(msg []byte) {
 	if msg == nil {
 		return
 	}
-	tt := make([]structures.PolyTrade, 0)
+	tt := make([]api.PolyTrade, 0)
 	err := json.Unmarshal(msg, &tt)
 	if err != nil {
 		log.Warn("error processing upstream message",
@@ -40,65 +50,30 @@ func TradeHandler(msg []byte) {
 	}
 
 	for _, rt := range tt {
-		t := structures.Trade{}
-		timestamp := rt.T
-		// parse timestamp - do this first to get the best lag measure
-		t.Timestamp = time.Unix(0, int64(1000000*float64(timestamp)))
-		lagOnReceipt := time.Now().Sub(t.Timestamp).Seconds()
-		var (
-			isOpen, isReOpen, isClose bool
-			isOffOpen, isOffClose     bool
-		)
-		for _, c := range rt.C {
-			switch c {
-			case ConditionExchangeSummary:
-				return
-			case ConditionReOpening:
-				isReOpen = true
-			case ConditionOpening:
-				isOpen = true
-			case ConditionClosing:
-				isClose = true
-			case OfficialConditionOpening:
-				isOffOpen = true
-			case OfficialConditionClosing:
-				isOffClose = true
-			}
+		if conditionsPresent(rt.Conditions) {
+			continue
 		}
 
-		t.Size = int64(rt.S)
-		t.Price = rt.P
+		// Polygon time is in milliseconds since the Unix epoch
+		timestamp := time.Unix(0, int64(1000*1000*float64(rt.Timestamp)))
+		lagOnReceipt := time.Now().Sub(timestamp).Seconds()
+		t := trade{
+			epoch: timestamp.Unix(),
+			nanos: int32(timestamp.Nanosecond()),
+			sz: int32(rt.Size),
+			px: float32(rt.Price),
+		}
 		// skip empty trades
-		if t.Size <= 0 || t.Price <= 0 {
+		if t.sz <= 0 || t.px <= 0 {
 			return
 		}
-
-		symbol := rt.Sym
-		var format string
-		switch {
-		case isOpen:
-			format = string(enum.OpeningPrice) + "%s"
-		case isReOpen:
-			format = string(enum.ReOpeningPrice) + "%s"
-		case isClose:
-			format = string(enum.ClosingPrice) + "%s"
-		case isOffOpen:
-			format = string(enum.OfficialOpeningPrice) + "%s"
-		case isOffClose:
-			format = string(enum.OfficialClosingPrice) + "%s"
-		default:
-			format = string(enum.Trade) + "%s"
-		}
-		symbol = fmt.Sprintf(format, strings.Replace(symbol, "/", ".", 1))
-
-		if !isMoreRecent(symbol, t.Timestamp) {
-			return
-		}
-
-		utils.TradeLagStats.Update(lagOnReceipt, symbol)
-
-		// throw the trade on the queue
-		raft.GetCache().Queue(symbol, t)
+		symbol := fmt.Sprintf("%s", strings.Replace(rt.Symbol, "/", ".", 1))
+		pkt := &writePacket{
+			io.NewTimeBucketKey(symbol + "/1Min/TRADE"),
+			&t,
+			}
+		Write(pkt)
+		_ = lagOnReceipt
 	}
 }
 
@@ -108,7 +83,7 @@ func QuoteHandler(msg []byte) {
 	if msg == nil {
 		return
 	}
-	qq := make([]structures.PolyQuote, 0)
+	qq := make([]api.PolyQuote, 0)
 	err := json.Unmarshal(msg, &qq)
 	if err != nil {
 		log.Warn("error processing upstream message",
@@ -117,47 +92,61 @@ func QuoteHandler(msg []byte) {
 		return
 	}
 	for _, rq := range qq {
-		q := structures.Quote{}
-		timestamp := rq.T
-		q.Timestamp = time.Unix(0, int64(1000000*float64(timestamp)))
-		lagOnReceipt := time.Now().Sub(q.Timestamp).Seconds()
-		symbol := rq.Sym
-		format := string(enum.Quote) + "%s"
-		symbol = fmt.Sprintf(format, strings.Replace(symbol, "/", ".", 1))
-
-		if !isMoreRecent(symbol, q.Timestamp) {
-			return
+		timestamp := time.Unix(0, int64(1000*1000*float64(rq.Timestamp)))
+		lagOnReceipt := time.Now().Sub(timestamp).Seconds()
+		q := quote{
+			epoch: timestamp.Unix(),
+			nanos: int32(timestamp.Nanosecond()),
+			bidPx:    float32(rq.BidPrice),
+			bidSz:    int32(rq.BidSize),
+			askPx:    float32(rq.AskPrice),
+			askSz:    int32(rq.AskSize),
 		}
+		symbol := fmt.Sprintf("%s", strings.Replace(rq.Symbol, "/", ".", 1))
+		pkt := &writePacket{
+			io.NewTimeBucketKey(symbol+"/1Min/QUOTE"),
+			&q,
+		}
+		Write(pkt)
 
-		// parse the quote fields
-		q.BidPrice = rq.Bp
-		q.AskPrice = rq.Ap
-		q.BidSize = int64(rq.Bs)
-		q.AskSize = int64(rq.As)
+		_ = lagOnReceipt
+	}
 
-		utils.QuoteLagStats.Update(lagOnReceipt, symbol)
+}
 
-		// throw the quote on the queue
-		raft.GetCache().Queue(symbol, q)
+func Bar(msg *nats.Msg, backfillM *sync.Map) {
+	// quickly parse the json
+	symbol, _ := jsonparser.GetString(msg.Data, "sym")
+
+	if strings.Contains(symbol, "/") {
+		return
+	}
+
+	open, _ := jsonparser.GetFloat(msg.Data, "o")
+	high, _ := jsonparser.GetFloat(msg.Data, "h")
+	low, _ := jsonparser.GetFloat(msg.Data, "l")
+	close, _ := jsonparser.GetFloat(msg.Data, "c")
+	volume, _ := jsonparser.GetInt(msg.Data, "v")
+	epochMillis, _ := jsonparser.GetInt(msg.Data, "s")
+
+	epoch := epochMillis / 1000
+
+	backfillM.LoadOrStore(symbol, &epoch)
+
+	tbk := io.NewTimeBucketKeyFromString(fmt.Sprintf("%s/1Min/OHLCV", symbol))
+	csm := io.NewColumnSeriesMap()
+
+	cs := io.NewColumnSeries()
+	cs.AddColumn("Epoch", []int64{epoch})
+	cs.AddColumn("Open", []float32{float32(open)})
+	cs.AddColumn("High", []float32{float32(high)})
+	cs.AddColumn("Low", []float32{float32(low)})
+	cs.AddColumn("Close", []float32{float32(close)})
+	cs.AddColumn("Volume", []int32{int32(volume)})
+	csm.AddColumnSeries(*tbk, cs)
+
+	if err := executor.WriteCSM(csm, false); err != nil {
+		log.Error("[polygon] csm write failure for key: [%v] (%v)", tbk.String(), err)
 	}
 }
 
-func isMoreRecent(symbol string, t time.Time) bool {
-	// if we have existing trade for this symbol, compare the incoming timestamp to see if this is newer
-	ttI := raft.GetCache().GetFromQueue(symbol)
-	var ts time.Time
-	switch val := ttI.(type) {
-	case structures.Quote:
-		ts = val.Timestamp
-	case structures.Trade:
-		ts = val.Timestamp
-	}
-	if ts.IsZero() {
-		return true
-	} else {
-		if ts.After(t) {
-			return false
-		}
-	}
-	return true
-}
