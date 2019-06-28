5240036b472fc1c83c9b1d0e506edc1c0715f9d7
Luke Lonergan <464941+Notargets@users.noreply.github.com>
2019-06-28 16:31:12 -0700

    WIP: use websocket API for Polygon plugin (#224)
    * WIP: use websocket API for Polygon plugin
    
    * WIP: refactor
    
    * WIP: refactor
    
    * Compiles
    
    * Bars handler
    
    * Working tests using built-in go testing
    
    * cleanup
    
    * Fix log messages

---
+++ contrib/polygon/api/websocket.go
@@ -0,0 +1,317 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+
+	"github.com/alpacahq/marketstore/utils/log"
+	"github.com/alpacahq/marketstore/utils/pool"
+)
+
+var (
+	AllTrades = NewSubscriptionScope(Trade, nil)
+	AllQuotes = NewSubscriptionScope(Quote, nil)
+	AllBars   = NewSubscriptionScope(Agg, nil)
+)
+
+type Subscription struct {
+	Servers  []*url.URL
+	Incoming chan interface{}
+	conn     *websocket.Conn
+	doneChan chan struct{} // send to this to stop listener
+	running  bool
+	scope    *SubscriptionScope
+	sync.Mutex
+}
+
+// servers := utils.Settings["WS_SERVERS"]
+func NewSubscription(t Prefix, symbols []string) (s *Subscription, err error) {
+	s = &Subscription{}
+	if err = s.setURLs(); err != nil {
+		return
+	}
+	s.scope = NewSubscriptionScope(t, symbols)
+	return
+}
+
+func (s *Subscription) getRunning() (state bool) {
+	s.Lock()
+	defer s.Unlock()
+	return s.running
+}
+
+func (s *Subscription) setRunning(state bool) {
+	s.Lock()
+	defer s.Unlock()
+	s.running = state
+}
+
+func (s *Subscription) Hangup() {
+	s.Lock()
+	defer s.Unlock()
+	if s.doneChan != nil {
+		s.doneChan <- struct{}{}
+	}
+	s.running = false
+}
+
+func (s *Subscription) IsActive() bool {
+	s.Lock()
+	defer s.Unlock()
+	return s.conn != nil
+}
+
+func (s *Subscription) connect() (err error) {
+	s.disconnect()
+	s.Lock()
+	defer s.Unlock()
+	var hresp *http.Response
+	s.conn, hresp, err = websocket.DefaultDialer.Dial(s.Servers[0].String(), nil)
+	if err != nil {
+		return
+	}
+	if hresp.StatusCode != 101 { // 101 means "changing protocol", meaning we're upgrading to a websocket
+		return fmt.Errorf("upstream connection failure, status_code: %d", hresp.StatusCode)
+	}
+	// Check to see we have a response from the connection
+	err = s.conn.SetReadDeadline(time.Now().Add(time.Second))
+	if err != nil {
+		return
+	}
+	resp := s.readMsg()
+	if !strings.Contains(resp, "connected") {
+		return fmt.Errorf("unable to verify good connection")
+	}
+	return
+}
+func (s *Subscription) disconnect() {
+	s.Lock()
+	defer s.Unlock()
+	if s.conn == nil {
+		log.Warn("connection was already nil")
+		return
+	}
+	err := s.conn.Close()
+	if err != nil {
+		log.Warn("error closing connection")
+	}
+
+	s.conn = nil
+	return
+}
+
+func (s *Subscription) readMsg() string {
+	_, p, err := s.conn.ReadMessage()
+	if err != nil {
+		log.Warn("error reading authentication response")
+	}
+	return string(p)
+}
+
+func (s *Subscription) subscribe() (connected bool) {
+	var (
+		err  error
+		resp string
+	)
+	/*
+		ws.send('{"action":"auth","params":"YOUR_API_KEY"}')
+		ws.send('{"action":"subscribe","params":"C.AUD/USD,C.USD/EUR,C.USD/JPY"}')
+	*/
+	authMsg := fmt.Sprintf("{\"action\":\"auth\",\"params\":\"%s\"}", apiKey)
+	subMsg := fmt.Sprintf("{\"action\":\"subscribe\", \"params\":\"%s\"}", s.scope.getSubScope())
+
+	// send the subscription message to the upstream
+	_ = s.conn.WriteMessage(websocket.TextMessage, []byte(authMsg))
+	resp = s.readMsg()
+	if strings.Contains(resp, "authenticated") {
+		log.Info("authenticated successfully {%s:%v}",
+			"feed", s.scope.getSubScope())
+	} else {
+		log.Info("unable to authenticate")
+		return false
+	}
+	err = s.conn.WriteMessage(websocket.TextMessage, []byte(subMsg))
+	resp = s.readMsg()
+	if strings.Contains(resp, "success") {
+		log.Info("subscribed {%s:%v}", "feed", s.scope.getSubScope())
+	} else {
+		log.Warn("upstream subscription failure {%s:%v,%s:%v,%s:%v}",
+			"data_type", s.scope.getSubScope(),
+			"response", resp,
+			"error", err)
+		return false
+	}
+	return true
+}
+
+func (s *Subscription) listen() {
+	var (
+		maxMessageSize = int64(2048000)
+		// Time allowed to write a message to the peer.
+		readWait = 10 * time.Second
+		// Time allowed to write a message to the peer.
+		writeWait = readWait
+		// Time allowed to read the next pong message from the upstream.
+		pongWait = readWait
+		// Send pings to peer with this period. Must be less than pongWait.
+		pingPeriod = time.Second
+		pingTicker *time.Ticker
+	)
+	s.doneChan = make(chan struct{})
+	for {
+	restartConnection:
+		// start the upstream websocket connection
+		err := s.connect()
+		if err != nil {
+			log.Warn("error connecting to upstream {%s:%v,%s:%v,%s:%v}",
+				"server", s.Servers[0].String(),
+				"subscription", s.scope.getSubScope(),
+				"error", err.Error())
+			time.Sleep(time.Second)
+			goto restartConnection // try again
+		}
+
+		if !s.subscribe() {
+			time.Sleep(time.Second)
+			goto restartConnection // try again
+		}
+
+		if pingTicker != nil {
+			pingTicker.Stop()
+		}
+		pingTicker = time.NewTicker(pingPeriod)
+
+		s.conn.SetReadLimit(maxMessageSize)
+		err = s.conn.SetReadDeadline(time.Time{})
+		if err != nil {
+			log.Warn("error initializing read loop for upstream, restarting... {%s:%v,%s:%v}",
+				"subscription", s.scope.getSubScope(),
+				"error", err.Error())
+			goto restartConnection
+		}
+		s.conn.SetPongHandler(func(string) (err error) {
+			err = s.conn.SetReadDeadline(time.Now().Add(pongWait))
+			if err != nil {
+				log.Warn("error in pong handler {%s:%v,%s:%v}",
+					"subscription", s.scope.getSubScope(),
+					"error", err.Error())
+			}
+			return nil
+		})
+		for {
+			select {
+			case <-s.doneChan:
+				goto cleanup
+			case <-pingTicker.C:
+				if err := s.conn.WriteControl(websocket.PingMessage, []byte{},
+					time.Now().Add(writeWait)); err != nil {
+					log.Warn("upstream websocket connection failure {%s:%v,%s:%v}",
+						"subscription", s.scope.getSubScope(),
+						"error", err.Error())
+					goto restartConnection
+				}
+			default:
+				err = s.conn.SetReadDeadline(time.Now().Add(readWait))
+				tt, p, err := s.conn.ReadMessage()
+				switch tt {
+				case -1: // "NoFrame" error from the websocket library
+					log.Warn("failed websocket connection, restarting... {%s:%v}",
+						"subscription", s.scope.getSubScope())
+					goto restartConnection
+				case websocket.BinaryMessage, websocket.PingMessage, websocket.PongMessage:
+					log.Warn("ignoring non text msg from upstream {%s:%v,%s:%v}",
+						"message len", len(p),
+						"message type", tt)
+					continue // ignore
+				case websocket.CloseMessage:
+					log.Warn("received websocket close message, restarting... {%s:%v,%s:%v}",
+						"subscription", s.scope.getSubScope())
+					goto restartConnection
+				}
+				if err != nil {
+					time.Sleep(time.Second)
+					if strings.Contains(err.Error(), "timeout") {
+						log.Info("timeout handling incoming message, restarting connection... {%s:%v,%s:%v}",
+							"subscription", s.scope.getSubScope(),
+							"error", err)
+						time.Sleep(time.Second)
+						goto restartConnection
+					}
+					log.Warn("error handling incoming message {%s:%v,%s:%v}",
+						"subscription", s.scope.getSubScope(),
+						"error", err)
+					continue
+				}
+				s.Incoming <- p
+			}
+		}
+	}
+
+cleanup:
+	s.disconnect()
+	pingTicker.Stop()
+	return
+}
+
+func (s *Subscription) setURLs() (err error) {
+	//"nats://nats1.polygon.io:31111, nats://nats2.polygon.io:31112, nats://nats3.polygon.io:31113"
+	urls := strings.Split(servers, ",")
+	if len(urls) < 1 {
+		return fmt.Errorf("empty servers string")
+	}
+
+	parameters := url.Values{}
+	parameters.Add("apiKey", apiKey)
+
+	u := make([]*url.URL, len(urls))
+	for i := range urls {
+		urls[i] = strings.Trim(urls[i], " ") + "/stocks"
+		u[i], err = url.Parse(urls[i])
+		u[i].RawQuery = parameters.Encode()
+	}
+	s.Servers = u
+	return
+}
+
+// Subscribe to a websocket connection for a given data type
+// by providing a channel that the messages will be
+// written to
+func (s *Subscription) Subscribe(handler func(msg []byte)) {
+	if s.getRunning() {
+		return
+	}
+	s.setRunning(true)
+
+	log.Info("subscribing to upstream Polygon")
+	log.Info("enabling ... {%s:%v}", "scope", s.scope.getSubScope())
+
+	s.Incoming = make(chan interface{}, 100) //sized to 10x the worker pool
+
+	// initialize & start the async worker pool
+	workerPool := pool.NewPool(10, func(msg interface{}) {
+		handler(msg.([]byte))
+	})
+
+	go workerPool.Work(s.Incoming)
+
+	// monitoring goroutine
+	go func() {
+		tick := time.NewTicker(time.Second)
+		for range tick.C {
+			log.Debug(
+				"channel status {%s:%v,%s:%v,%s:%v}",
+				"channel", s.scope.getSubScope(),
+				"goroutines", runtime.NumGoroutine(),
+				"depth", len(s.Incoming))
+		}
+	}()
+
+	go s.listen()
+}
--- contrib/polygon/handlers/handlers.go
+++ contrib/polygon/handlers/handlers.go
@@ -1,269 +1,155 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 	"strings"
-	"sync"
 	"time"
 
+	"github.com/alpacahq/marketstore/contrib/polygon/backfill"
+
+	"github.com/alpacahq/marketstore/contrib/polygon/api"
 	"github.com/alpacahq/marketstore/executor"
 	"github.com/alpacahq/marketstore/utils/io"
 	"github.com/alpacahq/marketstore/utils/log"
-	"github.com/buger/jsonparser"
-	"github.com/eapache/channels"
-	nats "github.com/nats-io/go-nats"
 )
 
-const ConditionExchangeSummary = 51
+const (
+	ConditionExchangeSummary = 51
+	OfficialConditionClosing = 15
+	OfficialConditionOpening = 16
+	ConditionClosing         = 17
+	ConditionReOpening       = 18
+	ConditionOpening         = 19
+)
 
-func Bar(msg *nats.Msg, backfillM *sync.Map) {
-	// quickly parse the json
-	symbol, _ := jsonparser.GetString(msg.Data, "sym")
+func conditionsPresent(conditions []int) (skip bool) {
+	for _, c := range conditions {
+		switch c {
+		case ConditionExchangeSummary, ConditionReOpening, ConditionOpening, ConditionClosing,
+			OfficialConditionOpening, OfficialConditionClosing:
+			return true
+		}
+	}
+	return
+}
 
-	if strings.Contains(symbol, "/") {
+// TradeHandler handles a Polygon WS trade
+// message and stores it to the cache
+func TradeHandler(msg []byte) {
+	if msg == nil {
+		return
+	}
+	tt := make([]api.PolyTrade, 0)
+	err := json.Unmarshal(msg, &tt)
+	if err != nil {
+		log.Warn("error processing upstream message",
+			"message", string(msg),
+			"error", err.Error())
 		return
 	}
 
-	open, _ := jsonparser.GetFloat(msg.Data, "o")
-	high, _ := jsonparser.GetFloat(msg.Data, "h")
-	low, _ := jsonparser.GetFloat(msg.Data, "l")
-	close, _ := jsonparser.GetFloat(msg.Data, "c")
-	volume, _ := jsonparser.GetInt(msg.Data, "v")
-	epochMillis, _ := jsonparser.GetInt(msg.Data, "s")
+	for _, rt := range tt {
+		if conditionsPresent(rt.Conditions) {
+			continue
+		}
 
-	epoch := epochMillis / 1000
-
-	backfillM.LoadOrStore(symbol, &epoch)
-
-	tbk := io.NewTimeBucketKeyFromString(fmt.Sprintf("%s/1Min/OHLCV", symbol))
-	csm := io.NewColumnSeriesMap()
-
-	cs := io.NewColumnSeries()
-	cs.AddColumn("Epoch", []int64{epoch})
-	cs.AddColumn("Open", []float32{float32(open)})
-	cs.AddColumn("High", []float32{float32(high)})
-	cs.AddColumn("Low", []float32{float32(low)})
-	cs.AddColumn("Close", []float32{float32(close)})
-	cs.AddColumn("Volume", []int32{int32(volume)})
-	csm.AddColumnSeries(*tbk, cs)
-
-	if err := executor.WriteCSM(csm, false); err != nil {
-		log.Error("[polygon] csm write failure for key: [%v] (%v)", tbk.String(), err)
+		// Polygon time is in milliseconds since the Unix epoch
+		timestamp := time.Unix(0, int64(1000*1000*float64(rt.Timestamp)))
+		lagOnReceipt := time.Now().Sub(timestamp).Seconds()
+		t := trade{
+			epoch: timestamp.Unix(),
+			nanos: int32(timestamp.Nanosecond()),
+			sz:    int32(rt.Size),
+			px:    float32(rt.Price),
+		}
+		// skip empty trades
+		if t.sz <= 0 || t.px <= 0 {
+			return
+		}
+		symbol := fmt.Sprintf("%s", strings.Replace(rt.Symbol, "/", ".", 1))
+		pkt := &writePacket{
+			io.NewTimeBucketKey(symbol + "/1Min/TRADE"),
+			&t,
+		}
+		Write(pkt)
+		_ = lagOnReceipt
 	}
 }
 
-func Trade(msg *nats.Msg) {
-	var skip = false
-
-	// get the condition in case we should ignore this quote
-	jsonparser.ArrayEach(msg.Data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		if c, _ := strconv.Atoi(string(value)); c == ConditionExchangeSummary {
-			skip = true
-		}
-	}, "c")
-
-	if skip {
+// QuoteHandler handles a Polygon WS quote
+// message and stores it to the cache
+func QuoteHandler(msg []byte) {
+	if msg == nil {
 		return
 	}
-
-	// parse symbol and swap / for .
-	symbol, _ := jsonparser.GetString(msg.Data, "sym")
-	symbol = strings.Replace(symbol, "/", ".", 1)
-	size, _ := jsonparser.GetInt(msg.Data, "s")
-	px, _ := jsonparser.GetFloat(msg.Data, "p")
-	epochMillis, _ := jsonparser.GetInt(msg.Data, "t")
-
-	timestamp := time.Unix(0, 1000*1000*epochMillis)
-
-	pkt := &writePacket{
-		io.NewTimeBucketKey(symbol + "/1Min/TRADE"),
-		&trade{
-			epoch: timestamp.Unix(),
-			nanos: int32(timestamp.Nanosecond()),
-			px:    float32(px),
-			sz:    int32(size),
-		}}
-
-	Write(pkt)
-}
-
-type trade struct {
-	epoch int64
-	nanos int32
-	px    float32
-	sz    int32
-}
-
-func Quote(msg *nats.Msg) {
-	// parse symbol and swap / for .
-	symbol, _ := jsonparser.GetString(msg.Data, "sym")
-	symbol = strings.Replace(symbol, "/", ".", 1)
-	bidPx, _ := jsonparser.GetFloat(msg.Data, "bp")
-	askPx, _ := jsonparser.GetFloat(msg.Data, "ap")
-	bidSize, _ := jsonparser.GetInt(msg.Data, "bs")
-	askSize, _ := jsonparser.GetInt(msg.Data, "as")
-	epochMillis, _ := jsonparser.GetInt(msg.Data, "t")
-
-	timestamp := time.Unix(0, 1000*1000*epochMillis)
-
-	pkt := &writePacket{
-		io.NewTimeBucketKey(symbol + "/1Min/QUOTE"),
-		&quote{
-			epoch: timestamp.Unix(),
-			nanos: int32(timestamp.Nanosecond()),
-			bidPx: float32(bidPx),
-			askPx: float32(askPx),
-			bidSz: int32(bidSize),
-			askSz: int32(askSize),
-		}}
-
-	Write(pkt)
-}
-
-type quote struct {
-	epoch int64   // 8
-	nanos int32   // 4
-	bidPx float32 // 4
-	askPx float32 // 4
-	bidSz int32   // 4
-	askSz int32   // 4
-}
-
-var (
-	w = &writer{
-		dataBuckets: map[io.TimeBucketKey]interface{}{},
-		interval:    100 * time.Millisecond,
-		c:           channels.NewInfiniteChannel(),
+	qq := make([]api.PolyQuote, 0)
+	err := json.Unmarshal(msg, &qq)
+	if err != nil {
+		log.Warn("error processing upstream message",
+			"message", string(msg),
+			"error", err.Error())
+		return
 	}
-	once sync.Once
-)
-
-type writePacket struct {
-	tbk  *io.TimeBucketKey
-	data interface{}
-}
-
-type writer struct {
-	sync.Mutex
-	dataBuckets map[io.TimeBucketKey]interface{}
-	interval    time.Duration
-	c           *channels.InfiniteChannel
-}
-
-func (w *writer) write() {
-	// preallocate the data structures for re-use
-	var (
-		csm io.ColumnSeriesMap
-
-		epoch []int64
-		nanos []int32
-		bidPx []float32
-		askPx []float32
-		px    []float32
-		bidSz []int32
-		askSz []int32
-		sz    []int32
-	)
-
-	for {
-		select {
-		case m := <-w.c.Out():
-			w.Lock()
-			packet := m.(*writePacket)
-
-			if bucket, ok := w.dataBuckets[*packet.tbk]; ok {
-				switch packet.data.(type) {
-				case *quote:
-					w.dataBuckets[*packet.tbk] = append(bucket.([]*quote), packet.data.(*quote))
-				case *trade:
-					w.dataBuckets[*packet.tbk] = append(bucket.([]*trade), packet.data.(*trade))
-				}
-			} else {
-				switch packet.data.(type) {
-				case *quote:
-					w.dataBuckets[*packet.tbk] = []*quote{packet.data.(*quote)}
-				case *trade:
-					w.dataBuckets[*packet.tbk] = []*trade{packet.data.(*trade)}
-				}
-			}
-
-			w.Unlock()
-
-		case <-time.After(w.interval):
-			w.Lock()
-			csm = io.NewColumnSeriesMap()
-
-			for tbk, bucket := range w.dataBuckets {
-				switch bucket.(type) {
-				case []*quote:
-					b := bucket.([]*quote)
-
-					for _, q := range b {
-						epoch = append(epoch, q.epoch)
-						nanos = append(nanos, q.nanos)
-						bidPx = append(bidPx, q.bidPx)
-						askPx = append(askPx, q.askPx)
-						bidSz = append(bidSz, q.bidSz)
-						askSz = append(askSz, q.askSz)
-					}
-
-					if len(epoch) > 0 {
-						csm.AddColumn(tbk, "Epoch", epoch)
-						csm.AddColumn(tbk, "Nanoseconds", nanos)
-						csm.AddColumn(tbk, "BidPrice", bidPx)
-						csm.AddColumn(tbk, "AskPrice", askPx)
-						csm.AddColumn(tbk, "BidSize", bidSz)
-						csm.AddColumn(tbk, "AskSize", askSz)
-
-						// trim the slices
-						epoch = epoch[:0]
-						nanos = nanos[:0]
-						bidPx = bidPx[:0]
-						bidSz = bidSz[:0]
-						askPx = bidPx[:0]
-						askSz = askSz[:0]
-						w.dataBuckets[tbk] = b[:0]
-					}
-				case []*trade:
-					b := bucket.([]*trade)
-
-					for _, t := range b {
-						epoch = append(epoch, t.epoch)
-						nanos = append(nanos, t.nanos)
-						px = append(px, t.px)
-						sz = append(sz, t.sz)
-					}
-
-					if len(epoch) > 0 {
-						csm.AddColumn(tbk, "Epoch", epoch)
-						csm.AddColumn(tbk, "Nanoseconds", nanos)
-						csm.AddColumn(tbk, "Price", px)
-						csm.AddColumn(tbk, "Size", sz)
-
-						// trim the slices
-						epoch = epoch[:0]
-						nanos = nanos[:0]
-						px = px[:0]
-						sz = sz[:0]
-						w.dataBuckets[tbk] = b[:0]
-					}
-				}
-			}
-
-			w.Unlock()
-
-			if err := executor.WriteCSM(csm, true); err != nil {
-				log.Error("[polygon] failed to write csm (%v)", err)
-			}
+	for _, rq := range qq {
+		timestamp := time.Unix(0, int64(1000*1000*float64(rq.Timestamp)))
+		lagOnReceipt := time.Now().Sub(timestamp).Seconds()
+		q := quote{
+			epoch: timestamp.Unix(),
+			nanos: int32(timestamp.Nanosecond()),
+			bidPx: float32(rq.BidPrice),
+			bidSz: int32(rq.BidSize),
+			askPx: float32(rq.AskPrice),
+			askSz: int32(rq.AskSize),
 		}
+		symbol := fmt.Sprintf("%s", strings.Replace(rq.Symbol, "/", ".", 1))
+		pkt := &writePacket{
+			io.NewTimeBucketKey(symbol + "/1Min/QUOTE"),
+			&q,
+		}
+		Write(pkt)
+
+		_ = lagOnReceipt
 	}
+
 }
 
-func Write(pkt *writePacket) {
-	once.Do(func() {
-		go w.write()
-	})
+func BarsHandler(msg []byte) {
+	if msg == nil {
+		return
+	}
+	am := make([]api.PolyAggregate, 0)
+	err := json.Unmarshal(msg, &am)
+	if err != nil {
+		log.Warn("error processing upstream message",
+			"message", string(msg),
+			"error", err.Error())
+		return
+	}
+	for _, bar := range am {
+		timestamp := time.Unix(0, int64(1000*1000*float64(bar.EpochMillis)))
+		lagOnReceipt := time.Now().Sub(timestamp).Seconds()
 
-	w.c.In() <- pkt
+		epoch := bar.EpochMillis / 1000
+
+		backfill.BackfillM.LoadOrStore(bar.Symbol, &epoch)
+
+		tbk := io.NewTimeBucketKeyFromString(fmt.Sprintf("%s/1Min/OHLCV", bar.Symbol))
+		csm := io.NewColumnSeriesMap()
+
+		cs := io.NewColumnSeries()
+		cs.AddColumn("Epoch", []int64{epoch})
+		cs.AddColumn("Open", []float32{float32(bar.Open)})
+		cs.AddColumn("High", []float32{float32(bar.High)})
+		cs.AddColumn("Low", []float32{float32(bar.Low)})
+		cs.AddColumn("Close", []float32{float32(bar.Close)})
+		cs.AddColumn("Volume", []int32{int32(bar.Volume)})
+		csm.AddColumnSeries(*tbk, cs)
+
+		if err := executor.WriteCSM(csm, false); err != nil {
+			log.Error("[polygon] csm write failure for key: [%v] (%v)", tbk.String(), err)
+		}
+
+		_ = lagOnReceipt
+	}
 }
