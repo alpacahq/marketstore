dc5d679e20aa950944324ba19629eae41a66b189
rymdluo <raymondluo@teashade.com>
2019-08-06 15:52:15 +0800

    dassd

--- quanatee/api_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo ${!POLYGON_API_KEY}
-printenv
-printf '%s\n' "${!POLYGON_API_KEY}"
-
-sed -i "s|<polygon_api_key>|${!POLYGON_API_KEY} |g" mkts.yml
-echo marketstore start --config mkts.yml
