4daa015dfd26e4b2fa98807c578ea5a9694eb9b2
rymdluo <raymondluo@teashade.com>
2019-08-13 19:39:05 +0800

    Update tiingo_crypto_prices.go

--- quanatee/plugins/tiingo_crypto_prices/tiingo_crypto_prices.go
+++ quanatee/plugins/tiingo_crypto_prices/tiingo_crypto_prices.go
@@ -101,7 +101,7 @@ func GetTiingoPrices(symbol string, from, to time.Time, realTime bool, period st
                         url.QueryEscape(from.Format("2006-1-2")))
     
     if !realTime {
-        url = fmt.Sprintf( url + "&endDate=" + url.QueryEscape(to.Format("2006-1-2")) )
+        url = url + "&endDate=" + url.QueryEscape(to.Format("2006-1-2"))
     }
     
 	client := &http.Client{Timeout: ClientTimeout}
