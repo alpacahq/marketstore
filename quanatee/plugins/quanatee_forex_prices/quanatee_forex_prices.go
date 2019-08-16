1735fc01a0fdd0355b308e115214b259703a886e
rymdluo <raymondluo@teashade.com>
2019-08-16 14:39:19 +0800

    Update quanatee_forex_prices.go

--- quanatee/plugins/quanatee_forex_prices/quanatee_forex_prices.go
+++ quanatee/plugins/quanatee_forex_prices/quanatee_forex_prices.go
@@ -169,11 +169,11 @@ func GetIntrinioPrices(symbol string, from, to time.Time, realTime bool, period 
 	}
     
     if startOfSlice > -1 && endOfSlice > -1 {
-        quote.Epoch = quote.Epoch[startOfSlice+1:endOfSlice+1]
-        quote.Open = quote.Open[startOfSlice+1:endOfSlice+1]
-        quote.High = quote.High[startOfSlice+1:endOfSlice+1]
-        quote.Low = quote.Low[startOfSlice+1:endOfSlice+1]
-        quote.Close = quote.Close[startOfSlice+1:endOfSlice+1]
+        quote.Epoch = quote.Epoch[startOfSlice:endOfSlice]
+        quote.Open = quote.Open[startOfSlice:endOfSlice]
+        quote.High = quote.High[startOfSlice:endOfSlice]
+        quote.Low = quote.Low[startOfSlice:endOfSlice]
+        quote.Close = quote.Close[startOfSlice:endOfSlice]
     } else {
         quote = NewQuote(symbol, 0)
     }
