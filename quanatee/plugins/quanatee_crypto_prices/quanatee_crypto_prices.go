9c1b468739917612cbd3843b4ec7d3a8cd74a02a
rymdluo <raymondluo@teashade.com>
2019-09-06 00:56:29 +0800

    Update quanatee_crypto_prices.go

--- quanatee/plugins/quanatee_crypto_prices/quanatee_crypto_prices.go
+++ quanatee/plugins/quanatee_crypto_prices/quanatee_crypto_prices.go
@@ -173,7 +173,7 @@ func GetTiingoPrices(symbol string, from, to, last time.Time, realTime bool, per
 // FetcherConfig is a structure of binancefeeder's parameters
 type FetcherConfig struct {
 	Symbols        []string `json:"symbols"`
-    Indices        string `json:"indices"`
+    Indices        map[string][]string `json:"indices"`
     ApiKey         string   `json:"api_key"`
 	QueryStart     string   `json:"query_start"`
 	BaseTimeframe  string   `json:"base_timeframe"`
