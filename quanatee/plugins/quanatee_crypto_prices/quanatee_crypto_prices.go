52fae739f81657ac0b2d46b7fc2f899020a2ee96
rymdluo <raymondluo@teashade.com>
2019-09-05 22:20:36 +0800

    Update quanatee_crypto_prices.go

--- quanatee/plugins/quanatee_crypto_prices/quanatee_crypto_prices.go
+++ quanatee/plugins/quanatee_crypto_prices/quanatee_crypto_prices.go
@@ -252,7 +252,7 @@ func NewBgWorker(conf map[string]interface{}) (bgworker.BgWorker, error) {
 	var queryStart time.Time
 	timeframeStr := "1Min"
 	var symbols []string
-	var symbols map[string][]string
+	var schema map[string][]string
 
 	if config.BaseTimeframe != "" {
 		timeframeStr = config.BaseTimeframe
