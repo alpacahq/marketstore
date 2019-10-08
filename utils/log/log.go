cdd57c2fbb5d0c153829bdfddef877bdd5c3db77
rymdluo <raymondluo@teashade.com>
2019-10-08 16:50:27 +0800

    Update log.go

--- utils/log/log.go
+++ utils/log/log.go
@@ -23,6 +23,14 @@ func init() {
 	zap.ReplaceGlobals(logger)
 }
 
+func Print(msg string, args ...interface{}) {
+    if len(args) > 0 {
+        zap.S().Infof(msg, args...)
+    } else {
+        zap.S().Info(msg)
+    }
+}
+
 func Debug(msg string, args ...interface{}) {
 	if logLevel <= DEBUG {
 		if len(args) > 0 {
