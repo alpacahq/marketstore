8317a0a34cfe58ac946003fc60ec222d307dba82
rymdluo <raymondluo@teashade.com>
2019-11-14 03:11:40 +0800

    fsadf

--- quanatee/plugins/quanatee_stock_prices/quanatee_stock_prices.go
+++ quanatee/plugins/quanatee_stock_prices/quanatee_stock_prices.go
@@ -145,6 +145,9 @@ func GetPolygonPrices(symbol string, from, to, last time.Time, realTime bool, pe
     
 	for bar := 0; bar < numrows; bar++ {
         dt := time.Unix(0, forexData.PriceData[bar].Timestamp * int64(1000000)) //Timestamp is in milliseconds    
+        // Tiingo calculates candles by the closing time. I.e. 1 Min from 13:00-13:01 = 13:01 Candle; 
+        // Whereas Polygon calculates candle by opening time. So we add up the difference to match up to Tiingo
+        dt = dt.Add(period.Duration)
         // Only add data collected between from (timeStart) and to (timeEnd) range to prevent overwriting or confusion when aggregating data
         if ( calendar.IsWorkday(dt.UTC()) && 
            (( int(dt.UTC().Weekday()) == 1 && dt.UTC().Hour() >= 7 ) || 
