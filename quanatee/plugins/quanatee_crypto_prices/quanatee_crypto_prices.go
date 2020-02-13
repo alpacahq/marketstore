02b12a52da0aa9a527f9763578b2be9832b4774c
Raymond Luo <rymdluo@users.noreply.github.com>
2020-02-14 01:13:02 +0800

    dsaf

--- quanatee/plugins/quanatee_crypto_prices/quanatee_crypto_prices.go
+++ quanatee/plugins/quanatee_crypto_prices/quanatee_crypto_prices.go
@@ -96,7 +96,7 @@ func GetPolygonPrices(symbol string, from, to, last time.Time, realTime bool, pe
     // https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/minute/2019-01-01/2019-02-01?unadjusted=true&apiKey=
     apiUrl := fmt.Sprintf(
                         "https://api.polygon.io/v2/aggs/ticker/%s/range/%s/minute/%s/%s?unadjusted=false&apiKey=%s",
-                        "X:"+symbol,
+                        "X%3A"+symbol,
                         resampleFreq,
                         url.QueryEscape(from.AddDate(0, 0, -1).Format("2006-01-02")),
                         url.QueryEscape(to.AddDate(0, 0, 1).Format("2006-01-02")),
