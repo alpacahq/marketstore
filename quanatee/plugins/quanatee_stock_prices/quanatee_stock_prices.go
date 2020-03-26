0e2f706cd13e617d8a5669f4e16f363b8d62d0a9
Raymond Luo <rymdluo@users.noreply.github.com>
2020-03-27 04:35:31 +0800

    dsaf

--- quanatee/plugins/quanatee_stock_prices/quanatee_stock_prices.go
+++ quanatee/plugins/quanatee_stock_prices/quanatee_stock_prices.go
@@ -106,7 +106,7 @@ func GetPolygonPrices(symbol string, from, to, last time.Time, realTime bool, pe
                         token)
     
     if !realTime {
-        time.Sleep(time.Millisecond*rand.Intn(1000))
+        time.Sleep(time.Millisecond*time.Duration(rand.Intn(1000)))
     }
     
 	client := &http.Client{Timeout: ClientTimeout}
@@ -260,7 +260,7 @@ func GetTiingoPrices(symbol string, from, to, last time.Time, realTime bool, per
     if !realTime {
         apiUrl = apiUrl + "&endDate=" + url.QueryEscape(to.Format("2006-1-2"))
         apiUrl2 = apiUrl2 + "&endDate=" + url.QueryEscape(to.Format("2006-1-2"))
-        time.Sleep(time.Millisecond*rand.Intn(1000))
+        time.Sleep(time.Millisecond*time.Duration(rand.Intn(1000)))
     }
     
 	client := &http.Client{Timeout: ClientTimeout}
@@ -733,7 +733,7 @@ func (tiieq *IEXFetcher) Run() {
                 }
             }
         } else {
-			time.Sleep(time.Millisecond*rand.Intn(1000))
+			time.Sleep(time.Millisecond*time.Duration(rand.Intn(1000)))
         }
 
 	}
