2472687f6417c1b282c63bbc394fc797c372ba92
Raymond Luo <rymdluo@users.noreply.github.com>
2020-03-27 20:26:47 +0800

    dsaf

--- quanatee/plugins/quanatee_forex_prices/quanatee_forex_prices.go
+++ quanatee/plugins/quanatee_forex_prices/quanatee_forex_prices.go
@@ -106,7 +106,7 @@ func GetPolygonPrices(symbol string, from, to, last time.Time, realTime bool, pe
                         token)
     
     if !realTime {
-        time.Sleep(time.Millisecond*time.Duration(rand.Intn(1000)))
+        time.Sleep(time.Millisecond*time.Duration(rand.Intn(500)))
     }
     
 	client := &http.Client{Timeout: ClientTimeout}
@@ -256,7 +256,7 @@ func GetTiingoPrices(symbol string, from, to, last time.Time, realTime bool, per
     
     if !realTime {
         apiUrl = apiUrl + "&endDate=" + url.QueryEscape(to.Format("2006-1-2"))
-        time.Sleep(time.Millisecond*time.Duration(rand.Intn(1000)))
+        time.Sleep(time.Millisecond*time.Duration(rand.Intn(500)))
     }
     
 	client := &http.Client{Timeout: ClientTimeout}
@@ -710,7 +710,7 @@ func (tiifx *ForexFetcher) Run() {
         } else {
             // log.Info("Forex written during backfill: %v", written)
             log.Info("Forex not written during backfill: %v", unwritten)
-			time.Sleep(time.Millisecond*time.Duration(rand.Intn(1000)))
+			time.Sleep(time.Millisecond*time.Duration(rand.Intn(500)))
         }
 
 	}
