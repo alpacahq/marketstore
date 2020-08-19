cec86f6c246908975d2d145a2357fbe63bc43e7f
Daito AKIMURA <dakimura@mercari.com>
2020-08-19 21:58:29 +0900

    refactor: remove Instance ID from Instance MetaData

---
+++ executor/wal/errors.go
@@ -0,0 +1,19 @@
+package wal
+
+import (
+	"fmt"
+	"github.com/alpacahq/marketstore/v4/utils/io"
+	"github.com/alpacahq/marketstore/v4/utils/log"
+)
+
+type ShortReadError string
+
+func (msg ShortReadError) Error() string {
+	return errReport("%s: Unexpectedly short read", string(msg))
+}
+
+func errReport(base string, msg string) string {
+	base = io.GetCallerFileContext(2) + ":" + base
+	log.Error(base, msg)
+	return fmt.Sprintf(base, msg)
+}
