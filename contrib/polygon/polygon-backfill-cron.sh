127ae1118158c2e2f30e16850911d1e2906eb6bf
leventeszamosi <levente@alpaca.markets>
2020-10-20 11:04:53 +0200

    helmchart for backfiller, and change to tick data

--- contrib/polygon/polygon-backfill-cron.sh
+++ contrib/polygon/polygon-backfill-cron.sh
@@ -2,17 +2,27 @@
 
 set -eEuo pipefail
 
-export BACKFILL_INTERVAL="${1}"  # e.g. '1 week ago'
-export BACKFILL_APIKEY="${2}"
-export BACKFILL_ARGS="${@:3}"
+export BACKFILL_SCHEDULE="${BACKFILL_SCHEDULE:=0 0 1 * * *}"
 export BACKFILL_PARALLELISM="${BACKFILL_PARALLELISM:=5}"
-export BACKFILL_MKTSDB_PATH="${BACKFILL_MKTSDB_PATH:=/data}"
+
+export BACKFILL_LIVE_DIR="${BACKFILL_LIVE_DIR:=/data/live}"
+export BACKFILL_OLD_DIR="${BACKFILL_OLD_DIR:=/data/old}"
+export BACKFILL_TEMP_DIR="${BACKFILL_TEMP_DIR:=/data/temp}"
+export BACKFILL_LOG_DIR="${BACKFILL_LOG_DIR:=/data/log}"
+
 : "${BACKFILL_SCHEDULE:=0 0 1 * * *}"
+
+mkdir -p ${BACKFILL_LIVE_DIR}
+mkdir -p ${BACKFILL_OLD_DIR}
+mkdir -p ${BACKFILL_TEMP_DIR}
+mkdir -p ${BACKFILL_LOG_DIR}
 
 backfill_job="$(dirname ${0})/polygon-backfill-job.sh"
 
 curl -L https://github.com/odise/go-cron/releases/download/v0.0.7/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
+
+[ -z "$BACKFILL_APIKEY" ] && echo "BACKFILL_APIKEY env not set" && exit 1
 
 /usr/local/bin/go-cron \
   -s="${BACKFILL_SCHEDULE}" \
--- contrib/polygon/polygon-backfill-job.sh
+++ contrib/polygon/polygon-backfill-job.sh
@@ -2,16 +2,51 @@
 
 set -eEuo pipefail
 
-from=$(date -d "-${BACKFILL_INTERVAL}" "+%Y-%m-%d")
+tradesFrom=$(date -d "${BACKFILL_TRADE_INTERVAL}" "+%Y-%m-%d")
+barsFrom=$(date -d "${BACKFILL_BAR_INTERVAL}" "+%Y-%m-%d")
+to=$(date -d "-1 day ago" "+%Y-%m-%d")
 now=$(date +%Y%m%d_%H%M%S)
-log_file="${BACKFILL_MKTSDB_PATH}/ingest-${now}.log"
+tradeLogFile="${BACKFILL_LOG_DIR}/ingest-trade-${now}.log"
+barLogFile="${BACKFILL_LOG_DIR}/ingest-bar-${now}.log"
+
+echo "Delete non live data"
+rm -rf ${BACKFILL_OLD_DIR}
+rm -rf ${BACKFILL_TEMP_DIR}
+mkdir -p ${BACKFILL_TEMP_DIR}
+
+echo "Start trade backfill"
 
 polygon_backfiller \
-  -from "${from}" \
+  -trades \
+  -from "${tradesFrom}" \
+  -to "${to}" \
   -parallelism "${BACKFILL_PARALLELISM}" \
-  ${BACKFILL_ARGS} \
+  ${BACKFILL_TRADE_ARGS} \
   -apiKey "${BACKFILL_APIKEY}" \
-  -dir "${BACKFILL_MKTSDB_PATH}" > "${log_file}"
+  -dir "${BACKFILL_TEMP_DIR}" > "${tradeLogFile}"
 
-echo "Triggering marketstore restart"
-kill $(pidof marketstore)
+echo "Start bar backfill"
+
+polygon_backfiller \
+  -bars \
+  -from "${barsFrom}" \
+  -to "${to}" \
+  -parallelism "${BACKFILL_PARALLELISM}" \
+  ${BACKFILL_BAR_ARGS} \
+  -apiKey "${BACKFILL_APIKEY}" \
+  -dir "${BACKFILL_TEMP_DIR}" > "${barLogFile}"
+
+pid=$(pidof marketstore)
+
+echo "Stopping marketstore"
+kill -STOP $pid
+
+echo "Move working folders"
+mv ${BACKFILL_LIVE_DIR} ${BACKFILL_OLD_DIR}
+mv ${BACKFILL_TEMP_DIR} ${BACKFILL_LIVE_DIR}
+
+echo "Kill marketstore"
+kill -9 $pid
+
+echo "Backfill ended successfully"
+
