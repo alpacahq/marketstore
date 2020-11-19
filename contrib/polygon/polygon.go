8253b2eaf5f7b69b175734a52f52ab8ba4e0cf15
Tibor Kiss <tibor.kiss@gmail.com>
2020-11-19 11:05:24 +0100

    Remove leftover struct

--- contrib/polygon/polygon.go
+++ contrib/polygon/polygon.go
@@ -25,24 +25,6 @@ type PolygonFetcher struct {
 	types  map[string]struct{} // Bars, Quotes, Trades
 }
 
-type FetcherConfig struct {
-	// polygon API key for authenticating with their APIs
-	APIKey string `json:"api_key"`
-	// polygon API base URL in case it is being proxied
-	// (defaults to https://api.polygon.io/)
-	BaseURL string `json:"base_url"`
-	// websocket servers for Polygon, default is: "wss://socket.polygon.io"
-	WSServers string `json:"ws_servers"`
-	// list of data types to subscribe to (one of bars, quotes, trades)
-	DataTypes []string `json:"data_types"`
-	// list of symbols that are important
-	Symbols []string `json:"symbols"`
-	// time string when to start first time, in "YYYY-MM-DD HH:MM" format
-	// if it is restarting, the start is the last written data timestamp
-	// otherwise, it starts from the latest streamed bar
-	QueryStart string `json:"query_start"`
-}
-
 // NewBgWorker returns a new instances of PolygonFetcher. See FetcherConfig
 // for more details about configuring PolygonFetcher.
 func NewBgWorker(conf map[string]interface{}) (w bgworker.BgWorker, err error) {
