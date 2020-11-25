596da9b0299968b050e59d2c7c7d0126f0d72ad5
Daito AKIMURA <dakimura@mercari.com>
2020-11-25 10:26:21 +0900

    add: integration test for coerce column feature

--- tests/integ/tests/test_coerce_column.py
+++ tests/integ/tests/test_coerce_column.py
@@ -9,11 +9,8 @@ import pandas as pd
 
 import pymarketstore as pymkts
 
-# Constants
-DATA_TYPE_TICK = [('Epoch', 'i8'), ('Bid', 'f4'), ('Ask', 'f4')]
-
-client = pymkts.Client(f"http://127.0.0.1:{os.getenv('MARKETSTORE_PORT',5993)}/rpc",
-                       grpc=(os.getenv("USE_GRPC", "false") == "true"))
+client = pymkts.Client(f"127.0.0.1:{os.getenv('MARKETSTORE_PORT',5998)}",
+                       grpc=(os.getenv("USE_GRPC", "true") == "true"))
 
 
 def timestamp(datestr):
@@ -22,12 +19,11 @@ def timestamp(datestr):
 
 @pytest.mark.parametrize('symbol, data_type, data, coerce_to', [
     ('TEST_COERCE', [('Epoch', 'i8'), ('Bid', 'f4'), ('Ask', 'f4')],
-     [(timestamp('2019-01-01 00:00:00'), 1.0, 2.0),  # epoch, Ask, Bid
-      (timestamp('2019-01-01 00:00:01'), 3.0, 4.0),
-      (timestamp('2019-12-31 23:59:59'), 5.0, 6.0)],
-     [('Epoch', 'i8'), ('Bid', 'f4'), ('Ask', 'f8')] # coerce f8 to f4
+     [(timestamp('2020-01-01 00:00:00'), 1.0, 2.0),  # epoch, Ask, Bid
+      (timestamp('2020-01-01 00:00:01'), 3.0, 4.0),
+      (timestamp('2020-01-01 00:00:02'), 5.0, 6.0)],
+     [('Epoch', 'i8'), ('Bid', 'f8'), ('Ask', 'f8')]  # coerce f8 to f4
      ),
-
 ])
 def test_column_coerce(symbol, data_type, data, coerce_to):
     # ---- given ----
@@ -37,12 +33,16 @@ def test_column_coerce(symbol, data_type, data, coerce_to):
     print(client.write(np.array(data, dtype=data_type), tbk, isvariablelength=False))
 
     # ---- when ----
-    reply = client.query(pymkts.Params(symbol, '1Sec', 'TICK', limit=10))
+    reply_before = client.query(pymkts.Params(symbol, '1Sec', 'TICK', limit=10))
 
-    # coerce columns
-    print(client.write(np.array(data, dtype=coerce_to), tbk, isvariablelength=False))
+    # write the same data but with different column dataType so that coerce_column happens
+    ret = client.write(np.array(data, dtype=coerce_to), tbk, isvariablelength=False)
+
+    reply_after = client.query(pymkts.Params(symbol, '1Sec', 'TICK', limit=10))
 
     # ---- then ----
-    data_without_epochs = [record[1:] for record in data]
-    print(reply.first().df().dtypes)
-    # assert (reply.first().df().values == data_without_epochs).all()
+    # no error (when there is no write error, json-rpc client returns "{'responses': None}" and gRPC client returns "")
+    assert str(ret) in ["{'responses': None}", ""]
+
+    # no data is updated
+    assert (reply_before.first().df().values == reply_after.first().df().values).all()
