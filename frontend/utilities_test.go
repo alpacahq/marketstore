f5c64e8432aa2ce2264165f1edb680017d61bff1
Daito AKIMURA <dakimura@mercari.com>
2021-01-25 09:25:32 +0900

    fix: unit test

--- frontend/utilities_test.go
+++ frontend/utilities_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"sync/atomic"
+	"time"
 
 	"github.com/alpacahq/marketstore/v4/utils"
 	. "gopkg.in/check.v1"
@@ -16,6 +17,7 @@ type HeartbeatTestSuite struct{}
 var _ = Suite(&HeartbeatTestSuite{})
 
 func (s *HeartbeatTestSuite) TestHandler(c *C) {
+	startTime := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
 	utils.Tag = "dev"
 	var TestValues = map[string]struct {
 		Recorder        *httptest.ResponseRecorder
@@ -38,7 +40,7 @@ func (s *HeartbeatTestSuite) TestHandler(c *C) {
 		switch key {
 		case "Success":
 			atomic.StoreUint32(&Queryable, uint32(1))
-			heartbeat(val.Recorder, nil)
+			NewUtilityAPIHandlers(startTime).heartbeat(val.Recorder, nil)
 			hm := HeartbeatMessage{}
 			err := json.NewDecoder(val.Recorder.Body).Decode(&hm)
 			if err != nil {
@@ -51,7 +53,7 @@ func (s *HeartbeatTestSuite) TestHandler(c *C) {
 			c.Assert(val.Recorder.Code, Equals, http.StatusOK)
 		case "Failure":
 			atomic.StoreUint32(&Queryable, uint32(0))
-			heartbeat(val.Recorder, nil)
+			NewUtilityAPIHandlers(startTime).heartbeat(val.Recorder, nil)
 			hm := HeartbeatMessage{}
 			err := json.NewDecoder(val.Recorder.Body).Decode(&hm)
 			if err != nil {
