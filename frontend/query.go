e6c4e11e266823cb712c6f9fa72f331f9c59c571
dakimura <34202807+dakimura@users.noreply.github.com>
2021-04-22 10:16:18 +0900

    refactor: separate executeSQL function (#441)

--- frontend/query.go
+++ frontend/query.go
@@ -92,152 +92,161 @@ func (s *DataService) Query(r *http.Request, reqs *MultiQueryRequest, response *
 	response.Version = utils.GitHash
 	response.Timezone = utils.InstanceConfig.Timezone.String()
 	for _, req := range reqs.Requests {
-		switch req.IsSQLStatement {
-		case true:
-			ast, err := sqlparser.NewAstBuilder(req.SQLStatement)
+		var (
+			resp *QueryResponse
+			err error
+		)
+		// SQL
+		if req.IsSQLStatement {
+			resp, err = s.executeSQL(req.SQLStatement)
 			if err != nil {
 				return err
 			}
-			es, err := sqlparser.NewExecutableStatement(s.catalogDir, ast.Mtree)
+		}else {
+			// Query
+			resp, err = s.executeQuery(&req)
 			if err != nil {
 				return err
 			}
-			cs, err := es.Materialize()
-			if err != nil {
-				return err
-			}
-			nds, err := io.NewNumpyDataset(cs)
-			if err != nil {
-				return err
-			}
-			tbk := io.NewTimeBucketKeyFromString(req.SQLStatement + ":SQL")
-			nmds, err := io.NewNumpyMultiDataset(nds, *tbk)
-			if err != nil {
-				return err
-			}
-			response.Responses = append(response.Responses,
-				QueryResponse{
-					nmds,
-				})
-
-		case false:
-			/*
-				Assumption: Within each TimeBucketKey, we have one or more of each category, with the exception of
-				the AttributeGroup (aka Record Format) and Timeframe
-				Within each TimeBucketKey in the request, we allow for a comma separated list of items, e.g.:
-					destination1.items := "TSLA,AAPL,CG/1Min/OHLCV"
-				Constraints:
-				- If there is more than one record format in a single destination, we return an error
-				- If there is more than one Timeframe in a single destination, we return an error
-			*/
-			dest := io.NewTimeBucketKey(req.Destination, req.KeyCategory)
-			/*
-				All destinations in a request must share the same record format (AttributeGroup) and Timeframe
-			*/
-			RecordFormat := dest.GetItemInCategory("AttributeGroup")
-			Timeframe := dest.GetItemInCategory("Timeframe")
-			Symbols := dest.GetMultiItemInCategory("Symbol")
-
-			if len(Timeframe) == 0 || len(RecordFormat) == 0 || len(Symbols) == 0 {
-				return fmt.Errorf("destinations must have a Symbol, Timeframe and AttributeGroup, have: %s",
-					dest.String())
-			} else if len(Symbols) == 1 && Symbols[0] == "*" {
-				// replace the * "symbol" with a list all known actual symbols
-				allSymbols := s.catalogDir.GatherCategoriesAndItems()["Symbol"]
-				symbols := make([]string, 0, len(allSymbols))
-				for symbol := range allSymbols {
-					symbols = append(symbols, symbol)
-				}
-				keyParts := []string{strings.Join(symbols, ","), Timeframe, RecordFormat}
-				itemKey := strings.Join(keyParts, "/")
-				dest = io.NewTimeBucketKey(itemKey, req.KeyCategory)
-			}
-
-			epochStart := int64(0)
-			epochEnd := int64(math.MaxInt64)
-			var epochStartNanos, epochEndNanos int64
-			if req.EpochStart != nil {
-				epochStart = *req.EpochStart
-				if req.EpochStartNanos != nil {
-					epochStartNanos = *req.EpochStartNanos
-				}
-			}
-			if req.EpochEnd != nil {
-				epochEnd = *req.EpochEnd
-				if req.EpochEndNanos != nil {
-					epochEndNanos = *req.EpochEndNanos
-				}
-			}
-			limitRecordCount := 0
-			if req.LimitRecordCount != nil {
-				limitRecordCount = *req.LimitRecordCount
-			}
-			limitFromStart := false
-			if req.LimitFromStart != nil {
-				limitFromStart = *req.LimitFromStart
-			}
-			columns := make([]string, 0)
-			if req.Columns != nil {
-				columns = req.Columns
-			}
-
-			start := io.ToSystemTimezone(time.Unix(epochStart, epochStartNanos))
-			end := io.ToSystemTimezone(time.Unix(epochEnd, epochEndNanos))
-			csm, err := executeQuery(
-				dest,
-				start, end,
-				limitRecordCount, limitFromStart,
-				columns,
-				s.catalogDir,
-			)
-			if err != nil {
-				return err
-			}
-
-			/*
-				Execute function pipeline, if requested
-			*/
-			if len(req.Functions) != 0 {
-				for tbkStr, cs := range csm {
-					csOut, err := runAggFunctions(req.Functions, cs, tbkStr, s.catalogDir)
-					if err != nil {
-						return err
-					}
-					csm[tbkStr] = csOut
-				}
-			}
-
-			/*
-				Separate each TimeBucket from the result and compose a NumpyMultiDataset
-			*/
-			var nmds *io.NumpyMultiDataset
-			for tbk, cs := range csm {
-				nds, err := io.NewNumpyDataset(cs)
-				if err != nil {
-					return err
-				}
-				if nmds == nil {
-					nmds, err = io.NewNumpyMultiDataset(nds, tbk)
-					if err != nil {
-						return err
-					}
-				} else {
-					nmds.Append(cs, tbk)
-				}
-			}
-
-			/*
-				Append the NumpyMultiDataset to the MultiResponse
-			*/
-
-			response.Responses = append(response.Responses,
-				QueryResponse{
-					nmds,
-				})
-
 		}
+
+		response.Responses = append(response.Responses, *resp)
 	}
 	return nil
+}
+
+func (s *DataService) executeSQL(sqlStatement string) (*QueryResponse, error) {
+	ast, err := sqlparser.NewAstBuilder(sqlStatement)
+	if err != nil {
+		return nil, err
+	}
+	es, err := sqlparser.NewExecutableStatement(s.catalogDir, ast.Mtree)
+	if err != nil {
+		return nil, err
+	}
+	cs, err := es.Materialize()
+	if err != nil {
+		return nil, err
+	}
+	nds, err := io.NewNumpyDataset(cs)
+	if err != nil {
+		return nil, err
+	}
+	tbk := io.NewTimeBucketKeyFromString(sqlStatement + ":SQL")
+	nmds, err := io.NewNumpyMultiDataset(nds, *tbk)
+	if err != nil {
+		return nil, err
+	}
+	return &QueryResponse{nmds}, nil
+}
+
+func (s *DataService) executeQuery(req *QueryRequest) (*QueryResponse, error) {
+	/*
+		Assumption: Within each TimeBucketKey, we have one or more of each category, with the exception of
+		the AttributeGroup (aka Record Format) and Timeframe
+		Within each TimeBucketKey in the request, we allow for a comma separated list of items, e.g.:
+			destination1.items := "TSLA,AAPL,CG/1Min/OHLCV"
+		Constraints:
+		- If there is more than one record format in a single destination, we return an error
+		- If there is more than one Timeframe in a single destination, we return an error
+	*/
+	dest := io.NewTimeBucketKey(req.Destination, req.KeyCategory)
+	/*
+		All destinations in a request must share the same record format (AttributeGroup) and Timeframe
+	*/
+	RecordFormat := dest.GetItemInCategory("AttributeGroup")
+	Timeframe := dest.GetItemInCategory("Timeframe")
+	Symbols := dest.GetMultiItemInCategory("Symbol")
+
+	if len(Timeframe) == 0 || len(RecordFormat) == 0 || len(Symbols) == 0 {
+		return nil, fmt.Errorf("destinations must have a Symbol, Timeframe and AttributeGroup, have: %s",
+			dest.String())
+	} else if len(Symbols) == 1 && Symbols[0] == "*" {
+		// replace the * "symbol" with a list all known actual symbols
+		allSymbols := s.catalogDir.GatherCategoriesAndItems()["Symbol"]
+		symbols := make([]string, 0, len(allSymbols))
+		for symbol := range allSymbols {
+			symbols = append(symbols, symbol)
+		}
+		keyParts := []string{strings.Join(symbols, ","), Timeframe, RecordFormat}
+		itemKey := strings.Join(keyParts, "/")
+		dest = io.NewTimeBucketKey(itemKey, req.KeyCategory)
+	}
+
+	epochStart := int64(0)
+	epochEnd := int64(math.MaxInt64)
+	var epochStartNanos, epochEndNanos int64
+	if req.EpochStart != nil {
+		epochStart = *req.EpochStart
+		if req.EpochStartNanos != nil {
+			epochStartNanos = *req.EpochStartNanos
+		}
+	}
+	if req.EpochEnd != nil {
+		epochEnd = *req.EpochEnd
+		if req.EpochEndNanos != nil {
+			epochEndNanos = *req.EpochEndNanos
+		}
+	}
+	limitRecordCount := 0
+	if req.LimitRecordCount != nil {
+		limitRecordCount = *req.LimitRecordCount
+	}
+	limitFromStart := false
+	if req.LimitFromStart != nil {
+		limitFromStart = *req.LimitFromStart
+	}
+	columns := make([]string, 0)
+	if req.Columns != nil {
+		columns = req.Columns
+	}
+
+	start := io.ToSystemTimezone(time.Unix(epochStart, epochStartNanos))
+	end := io.ToSystemTimezone(time.Unix(epochEnd, epochEndNanos))
+	csm, err := executeQuery(
+		dest,
+		start, end,
+		limitRecordCount, limitFromStart,
+		columns,
+		s.catalogDir,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		Execute function pipeline, if requested
+	*/
+	if len(req.Functions) != 0 {
+		for tbkStr, cs := range csm {
+			csOut, err := runAggFunctions(req.Functions, cs, tbkStr, s.catalogDir)
+			if err != nil {
+				return nil, err
+			}
+			csm[tbkStr] = csOut
+		}
+	}
+
+	/*
+		Separate each TimeBucket from the result and compose a NumpyMultiDataset
+	*/
+	var nmds *io.NumpyMultiDataset
+	for tbk, cs := range csm {
+		nds, err := io.NewNumpyDataset(cs)
+		if err != nil {
+			return nil, err
+		}
+		if nmds == nil {
+			nmds, err = io.NewNumpyMultiDataset(nds, tbk)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			nmds.Append(cs, tbk)
+		}
+	}
+
+	return &QueryResponse{nmds}, nil
 }
 
 type ListSymbolsResponse struct {
