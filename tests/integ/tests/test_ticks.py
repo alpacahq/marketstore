93e64fdbf824b5fa3bb23e02c05811fbd8d5cbea
Daito AKIMURA <dakimura@mercari.com>
2021-08-05 09:55:19 +0900

    fix(integtest): change astype to view

--- tests/integ/tests/test_ticks.py
+++ tests/integ/tests/test_ticks.py
@@ -29,7 +29,7 @@ def convert(data, with_nanoseconds=False):
           - add the nanesecond field to the index at query time: TODO
     """
     data = data.copy()
-    total_ns = data.index.astype(np.int64)
+    total_ns = data.index.view(np.int64)
 
     if with_nanoseconds:
         data['Nanoseconds'] = total_ns % (10 ** 9)
